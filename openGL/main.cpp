ccbb00b4d45544036eefb65d1f7c5c9c88fd3a6e
achyut neupane <achyutneupane@achyuts-MacBook-Pro-2.local>
2016-10-26 04:31:41 -0700

    Initial Commit

---
+++ openGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL
+//
+//  Created by achyut neupane on 10/26/16.
+//  Copyright © 2016 achyut neupane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
